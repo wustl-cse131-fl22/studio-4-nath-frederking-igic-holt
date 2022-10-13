bec10345498f76def637f7d2f9d6896732082213
natashaigic <natashaigic@localhost>
2022-10-13 13:57:17 -0500

    Flag Studio

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,32 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(220,200,220);
+		StdDraw.filledRectangle(.5,.5,.4,.25);
+		
+		StdDraw.setPenColor(240,240,200);
+		StdDraw.filledCircle(.5,.5,.25);
+		
+		StdDraw.setPenColor(140,220,160);
+		StdDraw.filledCircle(.5,.5,.18);
+		
+		StdDraw.setPenColor(150,200,220);
+		StdDraw.filledCircle(.5,.5,.1);
+		StdDraw.setPenColor(255,175,95);
+		StdDraw.filledCircle(.8,.65,0.05);
+		
+		StdDraw.setPenColor(255,175,95);
+		StdDraw.filledCircle(.2,.65,0.05);
+		
+		StdDraw.setPenColor(255,175,95);
+		StdDraw.filledCircle(.2,.35,0.05);
+		
+		StdDraw.setPenColor(255,175,95);
+		StdDraw.filledCircle(.8,.35,0.05);
+		
+		StdDraw.setPenColor(150,200,220);
+		StdDraw.setPenRadius(0.025);
+		StdDraw.rectangle(.5,.5, .4, .25);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,21 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		//pink rectangle
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		StdDraw.filledRectangle(x,y,halfWidth, halfHeight);
+			
+		
+		
+		
 	}
 }
